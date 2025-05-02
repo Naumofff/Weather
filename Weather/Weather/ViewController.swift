58f66bb3094d687dff2280336a7ecf7e4cf13877
Naumoff <il.naumov007@gmail.com>
2025-05-02 09:25:01 +0300

    addLoginForm

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -8,13 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var loginField: UITextField!
+    
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello")
+       
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("2")
     }
 
 
+    @IBAction func confirmButton(_ sender: Any) {
+        let login = loginField.text!
+        let password = passwordField.text!
+        
+        
+        if login == "admin" && password == "123456" {
+            mainLabel.text = "авторизация прошла успешно"
+        } else {
+            
+            mainLabel.text = "неуспешная авторизация"
+        }
+    }
+    @objc func keyboardWasShown(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        bottomConstraint.constant = frame.height
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        bottomConstraint.constant = 0
+    }
 }
 
